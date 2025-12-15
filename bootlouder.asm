0e6245005b059637984b39cc83166abdbf29516d
Enlvis <onedoorgame@gmail.com>
2025-12-15 13:33:50 +0100

    Update bootlouder.asm

--- bootlouder.asm
+++ bootlouder.asm
@@ -2,55 +2,57 @@
 [ORG 0x7C00]
 
 start:
-    ; ===== inicjalizacja =====
+    mov [boot_drive], dl
+
+    ; ustaw segmenty
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7C00
+    mov sp, 0x7000
 
-    ; ===== tekst startowy =====
+    ; komunikat startowy
     mov si, boot_msg
 .print:
     lodsb
-    cmp al, 0
-    je load_kernel
+    test al, al
+    jz load_kernel
     mov ah, 0x0E
+    mov bh, 0
+    mov bl, 0x0F
     int 0x10
     jmp .print
 
-; ===== ładowanie kernela =====
 load_kernel:
-    mov ah, 0x02      ; BIOS read
-    mov al, 2         ; liczba sektorów kernela
-    mov ch, 0
-    mov cl, 2         ; kernel zaczyna się od sektora 2
-    mov dh, 0
-    mov dl, 0x00      ; pierwszy dysk
-    mov bx, 0x1000    ; adres kernela
-
-    xor ax, ax
+    mov ax, 0x1000
     mov es, ax
+    xor bx, bx
 
+    mov ah, 0x02       ; BIOS read
+    mov al, 1          ; 1 sektor kernela
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, [boot_drive]
     int 0x13
     jc disk_error
 
-    ; ===== skok do kernela =====
-    jmp 0x0000:0x1000
+    jmp 0x1000:0x0000
 
-; ===== błąd dysku =====
 disk_error:
     mov si, err_msg
 .err:
     lodsb
-    cmp al, 0
-    je $
+    test al, al
+    jz $
     mov ah, 0x0E
+    mov bl, 0x0C
     int 0x10
     jmp .err
 
+boot_drive db 0
 boot_msg db "ProtonOS booting...",13,10,0
-err_msg  db "Disk error! Kernel not found.",0
+err_msg  db "Disk error!",0
 
 times 510-($-$$) db 0
 dw 0xAA55
